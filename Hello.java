c68e804254a18a8542acf5e0a0fb877e1adf84af
Prashant2468 <prashantpatil932870@gmail.com>
2025-02-06 22:14:58 +0530

    Third Commit

--- Hello.java
+++ Hello.java
@@ -1,9 +1,10 @@
-class Hello
-{
-	public static void main(String args[])
-	{
-		System.out.println("Prashant Patil");
-		System.out.println("I am MCA STudent");
 
-	}
+class Hello {
+
+    public static void main(String args[]) {
+        System.out.println("Prashant Patil");
+        System.out.println("I am MCA STudent");
+        System.out.println("I study in UTU");
+
+    }
 }
